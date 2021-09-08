cf668fe9c2af10c9efe2a0d505a9062b1bbb09be
sk421120 <sk421120@naver.com>
2021-09-08 16:45:54 +0900

    rememberList

---
+++ react-remember-list/src/comps/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>Remember List 2021</h1>
+    </header>
+  );
+}
+
+export default Header;
